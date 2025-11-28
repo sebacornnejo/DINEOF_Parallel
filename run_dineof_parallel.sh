7f2a510124b76e3918e933587be9a5b255853ddd
Sebastián E. Cornejo-Guzmán <121766445+sebacornnejo@users.noreply.github.com>
2025-11-28 11:50:49 -0300

    Update LD_LIBRARY_PATH to use home directory

--- run_dineof_parallel.sh
+++ run_dineof_parallel.sh
@@ -4,7 +4,7 @@
 # Set number of threads
 export OMP_NUM_THREADS=16
 export OPENBLAS_NUM_THREADS=16
-export LD_LIBRARY_PATH=/home/sebastian.cornejo/miniconda3/envs/dineof_env/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=~/miniconda3/envs/dineof_env/lib:$LD_LIBRARY_PATH
 
 # Run DINEOF
 cd DINEOF_Parallel
